22b45e248aebbdd5309629db42ba43514eef2fd3
Rickard Olsson <rickard.olsson@hey.com>
2021-12-01 21:15:45 +0100

    Add part 2

--- day1.rb
+++ day1.rb
@@ -6,5 +6,14 @@ def increments(numbers)
   increments.count
 end
 
+def three_measurements(numbers)
+  numbers.each_cons(3).map(&:sum)
+end
+
 input = (File.readlines 'day1_puzzle_input.txt').map!(&:to_i)
-puts increments(input)
+
+part1 = increments(input)
+puts "Part 1: #{part1}"
+
+part2 = increments(three_measurements(input))
+puts "Part 2: #{part2}"
