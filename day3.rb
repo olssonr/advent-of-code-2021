1f1afed4e69c965e36fbe4c5d81fb8c57f19a292
Rickard Olsson <rickard.olsson@hey.com>
2021-12-04 18:05:08 +0100

    Refactored day3 part1 with custom classes

--- day3.rb
+++ day3.rb
@@ -1,28 +1,68 @@
 # frozen_string_literal: true
 
-# Since ruby does not provide us with an easy way to use the bitwise not
-# on a binary number we need to implement our own, it works on signed
-# integers and to_s appends - to the number, which is not what we want.
-# This is simple enough use XOR and a bitmask.
-def bitwise_not(binary_string)
-  length = binary_string.length
-  negation_bit_mask = ("1" * length)
-  binary_string.to_i(2) ^ negation_bit_mask.to_i(2)
+# Representation of a binary number, based on arrays to utilize transpose
+class BinaryNumber < Array
+  # TOOD: Fix so doesn't lose BinaryNumber class when doing array operations
+  def self.from_array(array)
+    new.concat(array)
+  end
+
+  def self.from_string(string)
+    new(string.chars.map(&:to_i))
+  end
+
+  def to_s
+    join
+  end
+
+  def to_i
+    to_s.to_i(2)
+  end
+
+  # Since ruby does not provide us with an easy way to use the bitwise not
+  # on a binary number we need to implement our own, it works on signed
+  # integers and to_s appends - to the number, which is not what we want.
+  # This is simple enough use XOR and a bitmask.
+  def bitwise_not
+    negation_bit_mask = self.class.new(length, 1)
+    to_i ^ negation_bit_mask.to_i
+  end
 end
 
-binary_strings = (File.readlines 'day3_puzzle_input.txt').map(&:chomp)
-binary_length = binary_strings.first.length
+# Representation of a report from a submarine specialize for finding sleigh keys
+class DiagnosticReport
+  def initialize(binary_matrix)
+    @binary_matrix = binary_matrix
 
-one_counter = Array.new(binary_length, 0)
-binary_strings.map do |binary_string|
-  binary_string.chars.each_with_index { |char, index| one_counter[index] += char.to_i }
+    # TOOD: Fix so doesn't lose BinaryNumber class when doing array operations
+    @gamma_rate_binary_number = BinaryNumber.from_array(most_common_bits_in_matrix)
+  end
+
+  def most_common_bits_in_matrix
+    @binary_matrix.transpose.map { |bits_in_position| most_common_bit(bits_in_position) }
+  end
+
+  # TODO: Didn't bother checking if BinaryNumber here, just refered to as bit_array
+  # Since in this case this refers to the first bit in all the binary numbers it might be
+  # confusing to refer to it as BinaryNumber, probably is just an array, but better check
+  def most_common_bit(bit_array)
+    bit_array.tally.max_by(&:last).first
+  end
+
+  def gamma_rate
+    @gamma_rate_binary_number.to_s.to_i(2)
+  end
+
+  def epsilon_rate
+    @gamma_rate_binary_number.bitwise_not
+  end
+
+  def power_consumption
+    gamma_rate * epsilon_rate
+  end
 end
 
-# TODO: Learn how to work with binaries in ruby, this is silly
-gamma_rate_binary_string = one_counter.map { |num| num > binary_strings.count / 2 ? "1" : "0" }.join
+binary_numbers = (File.readlines 'day3_puzzle_input.txt').map { |line| BinaryNumber.from_string(line.chomp) }
+report = DiagnosticReport.new(binary_numbers)
 
-gamma_rate = gamma_rate_binary_string.to_i(2)
-epsilon_rate = bitwise_not(gamma_rate_binary_string)
-power_consumption = gamma_rate * epsilon_rate
-
-puts "Part 1: #{power_consumption}"
+puts "Part1: #{report.power_consumption}"
