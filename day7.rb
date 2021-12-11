4de9a57be3b00dbc9fd89ba3adfa0af98b7976c4
Rickard Olsson <rickard.olsson@hey.com>
2021-12-11 22:51:59 +0100

    Day7 part2

--- day7.rb
+++ day7.rb
@@ -6,18 +6,29 @@ class CrabSubmarine
     @position = position
   end
 
-  def fuel_cost_to(position)
+  def distance_to(position)
     (@position - position).abs
+  end
+
+  def fuel_cost_to(position)
+    (1..distance_to(position)).sum
   end
 end
 
 # A position which a crab can move to. Given a list of crabs it can calculate their cost to move here
 class Position
-  attr_reader :cost
+  attr_reader :cost, :distance
 
   def initialize(value, crab_submarines)
     @value = value
+    @distance = calculate_distance(crab_submarines)
     @cost = calculate_cost(crab_submarines)
+  end
+
+  def calculate_distance(crab_submarines)
+    crab_submarines.sum do |crab_submarine|
+      crab_submarine.distance_to(@value)
+    end
   end
 
   def calculate_cost(crab_submarines)
@@ -34,6 +45,8 @@ end
 values = File.readlines('day7_puzzle_input.txt').first.split(',').map(&:to_i)
 crabs = values.map { |value| CrabSubmarine.new(value) }
 positions = values.uniq.map { |value| Position.new(value, crabs) }
+position_with_lowest_distance = positions.min_by(&:distance)
 position_with_lowest_cost = positions.min_by(&:cost)
 
-puts "Part1: #{position_with_lowest_cost.cost}"
+puts "Part1: #{position_with_lowest_distance.distance}"
+puts "Part2: #{position_with_lowest_cost.cost}"
